ed933fe499c2e52a3a223ebdbddac35608bda6da
254Unice <mumounice@gmail.com>
2022-04-08 11:56:42 +0300

    tash 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#!/bin/bash 
 #include <stdio.h>
-
 /**
 *main - function begins
 *
 * Return: function ends
 */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
