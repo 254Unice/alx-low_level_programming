483ef7da24f2a68b4b0f3ff237a0e14281ae3c5c
254Unice <mumounice@gmail.com>
2022-04-09 22:43:42 +0300

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,1 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main - entry point
- *
- * Description: Prints a number and it's last digit along with whether its
- * last digit is 0, greater than 5, or less than 6 and not 0.
- * Returns: 0
- * Return: 0
- */
-int main(void)
-{
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
