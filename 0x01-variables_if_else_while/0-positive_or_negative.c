7fb2174b5a11d084d15aa6e73693dc41632d8b23
254Unice <mumounice@gmail.com>
2022-04-09 21:18:52 +0300

    task 0 Variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-#inslude <stdio.h>
+#include <stdio.h>
 /**
-*main - Entry point
-*
-*Return: Always 0 (Successs/correct)
-*/
+ * main - entry point
+ *
+ * Description: Prints a number and it's last digit along with whether its
+ * last digit is 0, greater than 5, or less than 6 and not 0.
+ * Returns: 0
+ * Return: 0
+ */
 int main(void)
-int n;
-strand(time(0);
-n = rand() - RAND_MAX / 2;
-if (n==0)
-printf("%i is zero\n", n);
-else if (n<0))
-printf("%i is negative\n", n);
-else
-printf(:%i is positive\n", n);
-return (0);
-}
+{
