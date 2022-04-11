4227038ac78943a053406c113d4887372eec120b
254Unice <mumounice@gmail.com>
2022-04-11 19:43:01 +0300

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-
 /**
- * main - prints the letters of the alphabets
- *
- * Return: 0
- */
-
+*main - Entry point
+*
+*Return: Always 0 (success/corrrect)
+*/
 int main(void)
 {
-/Declarations/
-        char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-        putchar(letter);
-        }
-        putchar('\n');
-        return (0);
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
 }
